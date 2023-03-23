096ac70ac0f168a5de72182f9790c83a478230fd
Ayman <aymanbdouzi@gmail.com>
2023-03-23 19:40:51 +0000

    Created starter migrations

---
+++ database/migrations/2023_03_23_193707_create_progresses_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('progresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('ProgressStatus');
+            $table->string('Notes');
+            $table->foreignId('StudentId')->constrained('students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('progresses');
+    }
+};
