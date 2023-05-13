0e58bee69bc621564050cb2e8887b2c41ca8ffac
Ayman Badouzi <aymanbdouzi@gmail.com>
2023-05-13 15:03:14 +0100

    Contact
    Now the mails are saved to the database aswell

--- app/Http/Controllers/contactController.php
+++ app/Http/Controllers/contactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\contactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class contactController extends Controller
@@ -26,6 +27,16 @@ class contactController extends Controller
         ];
 
         if (Mail::to('aymanbdouzi@gmail.com')->send(new contactMail($mailData))) {
+            // Insert into database (mails table)
+            DB::table('mails')->insert([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'subject' => $validatedData['subject'],
+                'message' => $validatedData['message'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+
             return redirect()->route('main', '#contact-section')->with('success', 'Votre message a été envoyé avec succès! Nous vous répondrons dans les plus brefs délais.');
         } else {
             return redirect()->route('main', '#contact-section')->with('error', 'Quelque chose s\'est mal passé! Veuillez réessayer.');
