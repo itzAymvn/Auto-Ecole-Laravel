306b17db663973d0a6f02e93fbfd73667ca6ef20
Ayman Badouzi <aymanbdouzi@gmail.com>
2023-06-10 21:32:08 +0100

    Now it checks if there is a database before defining gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -10,53 +10,45 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
+    protected $policies = [];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
+        try {
+            // Check if the database connection is available
+            DB::connection()->getPdo();
 
-        // We first get all the permissions defined in the database
-        $permissions = DB::table('permissions')->where('status', 1)->get();
-        $transformedPermissions = [];
+            // Check if the permissions table exists
+            if (Schema::hasTable('permissions')) {
+                // Retrieve the permissions from the database
+                $permissions = DB::table('permissions')->where('status', 1)->get();
+                $transformedPermissions = [];
 
-        // We loop through the permissions
-        foreach ($permissions as $permission) {
+                // Transform the permissions data
+                foreach ($permissions as $permission) {
+                    $permissionName = $permission->permission . '-' . $permission->resource;
 
-            // We set the name of the permission as the key of the array (e.g. view-users)
-            $permissionName = $permission->permission . '-' . $permission->resource;
+                    if (!isset($transformedPermissions[$permissionName])) {
+                        $transformedPermissions[$permissionName] = [
+                            'name' => $permissionName,
+                            'roles' => [],
+                        ];
+                    }
 
-            // We check if the permission is not already defined
-            if (!isset($transformedPermissions[$permissionName])) {
+                    $transformedPermissions[$permissionName]['roles'][] = $permission->role;
+                }
 
-                // If not, we add it to the array with its name and an empty array of roles
-                $transformedPermissions[$permissionName] = [
-                    'name' => $permissionName,
-                    'roles' => [],
-                ];
+                $transformedPermissions = array_values($transformedPermissions);
+
+                // Define gates for each permission
+                foreach ($transformedPermissions as $permission) {
+                    Gate::define($permission['name'], function ($user) use ($permission) {
+                        return in_array($user->type, $permission['roles']);
+                    });
+                }
             }
-
-            // We push the role to the roles array of the permission
-            $transformedPermissions[$permissionName]['roles'][] = $permission->role;
-        }
-
-        // We re-index the array
-        $transformedPermissions = array_values($transformedPermissions);
-
-        // Define gates for each permission
-        foreach ($transformedPermissions as $permission) {
-            Gate::define($permission['name'], function ($user) use ($permission) {
-                return in_array($user->type, $permission['roles']);
-            });
+        } catch (\Exception $e) {
+            // Handle any exceptions that occur
         }
     }
 }
